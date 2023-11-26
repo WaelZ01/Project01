c08f5276b9026a83e1e5617475797f43b6a99e4f
WaelZ01 <149936969+WaelZ01@users.noreply.github.com>
2023-11-26 10:56:15 +0000

    project01

---
+++ readme.md
@@ -0,0 +1,6 @@
+-Memory Card geme
+-it is a game which you memorize the card places and try to get the most card matching
+-the game containes two player each player have two click,
+if the two click matched the player got a point
+-who got the most point wins 
+//the win points is out of 8
--- script.js
+++ script.js
@@ -1,52 +1,127 @@
-// Set the initial click count
-let cardFlipCount = 1;
-let flippedCard;
- 
- //attach a flip event to the cards
-        const cards = document.querySelectorAll('.card');
-    
-// get the front face of the clicked card
-         function flipCard() { 
-        this.querySelector('.frontFace').classList.add('flip') 
+const cards = document.querySelectorAll('.card');
+const  restathButton = document.querySelectorAll('.button');
+let flippedCard = false;
+let card1 , card2;
+let count = 0;
+let currentPlayer = 'player01';
+let playerOneScore = 0;
+let playerTwoScore = 0;
+
+
+//flip card action
+function flipCard(){
+        count++;
         
-// If first flip
+if(count > 2){
+        return;
+}
 
-
-//once clicked the card the card flip 
-
-
-        // Add one to card flip count
-
-        // Set the flippedCard
-
- // else
-//second click on another card  for check matching
-
-
-
-
-        //if the two card matched they stay appeard
-
-                // Get the src of the card clicked on
-                const card2src = this.querySelector('.frontFace').getAttribute('src');
-
-                // Get the src of the previously flipped card
-                flippedCard
-
-                // If the two src's are equal
-
-
-                // Else
-
-                        //(display none for both)
-                
-                        //if the two card is not matched they flip on there back again
-
-
-
-        // set the card flip count to 1
-        // Empty the flippedCard
-
+//toggle means if the class is there add it if not remove it same as on and off
+        this.classList.toggle('flip');
+        
+//first click 
+        if(!flippedCard){
+        flippedCard = true;
+        card1 = this;//(this) it will call the object 
         }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
+//seconed click
+        else{
+        flippedCard = false;
+        card2 = this;
+//check if it match ther card:
+        checkMatching();
+        // change player
+        if(currentPlayer === 'player01'){
+                currentPlayer = 'player02';
+        }else{
+                currentPlayer = 'player01';
+        }
+        }
+        }
+        // if we check matching never swich
+        function checkMatching(){
+                if(card1.dataset.cards === card2.dataset.cards && card1.id !== card2.id){//to get the data typr from html
+                        // if player 1, add to player 1 score
+                        if(currentPlayer == 'player01'){
+                                playerOneScore++;
+                        
+                        }
+                        // else
+                                // add to player 2 score
+                        else{
+                                playerTwoScore++;
+                        }
+                        if(playerOneScore + playerTwoScore == 8){
+                                checkForWinner();
+                        }
+
+                //if the card match each other
+                card1.removeEventListener('click',flipCard);
+                card2.removeEventListener('click',flipCard);
+                count = 0;
+                } 
+                //if the card not matched flip the cards
+                else(
+                setTimeout(()=>{
+                card1.classList.remove('flip');
+                card2.classList.remove('flip');
+                count = 0;
+                        },1000)
+                                );
+                document.querySelector('#player01').innerHTML = playerOneScore;
+                document.querySelector('#player02').innerHTML = playerTwoScore;     
+     
+
+                                      }                               
+//when the user prsses on the restart button 
+        function restart(){
+        cards.forEach(function(card){
+        card.classList.remove('flip')
+        })
+        setTimeout(shuffle, 1750);
+        }
+
+// shuffling the cards order 
+        function shuffle() {         
+//to not get the same order in every time         
+        let randomOrder = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+        cards.forEach(function(card){
+//give a random index order for random array
+        let index = Math.floor(Math.random() * randomOrder.length - 1);
+        card.style.order = randomOrder[index];
+                                                 })
+                                                        };
+
+// when the user clicks on the RESTART button the page will reload  
+//once clicked on restart button the page refreshs and shuffles the cards
+        restathButton.forEach(button => {
+        button.addEventListener('click', function() {
+        location.reload();
+                        });
+                                });
+
+//the check for winner function 
+
+function checkForWinner(){
+       if(playerOneScore > playerTwoScore){
+                setTimeout(function(){alert("PLAYER 1 WINS!!")},1000);
+        }else if(playerOneScore < playerTwoScore){
+                setTimeout(function(){alert("PLAYER 2 WINS!!")},1000);
+         }
+         else{
+                setTimeout(function(){alert("DRAW")},1000);
+
+         }
+
+}
+                        
+//to reload the page once the user press on the restart button                
+        Window.onload = (shuffle());
+//to select the class and applay on it the event listner (restart)
+        document.querySelector('.button').addEventListener('click',restart);
+//to add the click event to my function ( flipCard )
+        cards.forEach(card => card.addEventListener('click', flipCard));
+        const scoreElement = document.querySelector('player01');
+
+
